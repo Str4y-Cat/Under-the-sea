4f24187609da6fbf5c6a9707a7edb62955a58028
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-16 11:48:58 +0200

    ray plotting debugger

--- Boids 3d/src/rayScripts/RayPlotter.js
+++ Boids 3d/src/rayScripts/RayPlotter.js
@@ -1,21 +1,7 @@
 import * as THREE from 'three'
 import { lerp } from 'three/src/math/MathUtils.js'
 
-// export default
-// class RayPlotter{
 
-//     constructor()
-//     {
-
-//     }
-
-
-    
-
-    
-
-
-// }
 
 export function fibonacci_sphere(samples){
     const points = new Float32Array(samples*3)
@@ -58,6 +44,42 @@ export function fibonacci_sphere(samples){
     return points
 }
 
+export function fibonacci_sphere_vec3(samples, cutoff){
+    const points = []
+    const phi = Math.PI * (Math.sqrt(5)-1)  //golden angle in radians
+    // const n= 8
+    
+    
+    for(let i=0; i<samples; i++)
+        {
+           
+            let y = 1 - (i / (samples - 1)) * 2  // y goes from 1 to -1
+            let radius = Math.sqrt(1 - y * y)  // radius at y
+    
+            let theta = phi * i  // golden angle increment
+    
+            let x = Math.cos(theta) * radius 
+            let z = Math.sin(theta) * radius 
+
+
+                const i3=i*3
+                let shrinkFactor= 3
+
+                if(z<cutoff){
+                    // console.log("pushing")
+                    points.push(new THREE.Vector3(x/3,y/3,z/3))
+                }
+                else
+                {
+                    // console.log("cutting")
+                }
+            
+        }
+        // console.log("done")
+    // console.log(points)
+    return points
+}
+
 export function fibonacci_colours(samples, cutoff){
     const points = new Float32Array(samples*3)
     const phi = Math.PI * (Math.sqrt(5)-1)  //golden angle in radians
@@ -78,7 +100,7 @@ export function fibonacci_colours(samples, cutoff){
             const i3=i*3
             let shrinkFactor= 3
 
-            const color1=new THREE.Color("red")
+            const color1=new THREE.Color("black")
             const color2=new THREE.Color("green")
             let mixedColor= color1.clone()
             
@@ -94,3 +116,28 @@ export function fibonacci_colours(samples, cutoff){
         }
         return points
 }
+
+
+
+// export function lineGradient(base,target)
+// {   
+
+//     const colors= new Float32Array(vec3Array.length*3)
+
+//     for(let i=0; i<vec3Array.length;i++)
+//         {
+//             const i3=i*3
+
+//             const color1=new THREE.Color("red")
+//             const color2=new THREE.Color("green")
+//             let mixedColor= color1.clone()
+            
+//             const lerpValue= (z<cutoff)?1:0
+//             // console.log(lerpValue)
+//             mixedColor.lerp(color2,(lerpValue)) 
+
+//             points[i3]=mixedColor.r
+//             points[i3+1]=mixedColor.g
+//             points[i3+2]=mixedColor.b
+//         }
+// }
--- Boids 3d/src/script.js
+++ Boids 3d/src/script.js
@@ -6,6 +6,8 @@ import GUI from 'lil-gui'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
 import BoidController from './boidScripts/BoidController'
 import * as RAYS from './rayScripts/RayPlotter'
+// import { vec3 } from 'three/examples/jsm/nodes/Nodes.js'
+import { DragControls } from 'three/addons/controls/DragControls.js';
 
 
 
@@ -69,8 +71,23 @@ const floor= new THREE.Mesh(
 floor.rotation.x=-Math.PI/2
 floor.position.y-=debug.floorSize/2
 // floor.position.x=1
-
 // scene.add(floor)
+
+
+/**add drag objects
+ * 
+ */
+
+const dragMaterial= new THREE.MeshMatcapMaterial({matcap:matCapTexture})
+const dragGeometry1= new THREE.BoxGeometry(0.4,0.4,0.4)
+const dragGeometry2=  new THREE.CapsuleGeometry( 0.2, 0.2, 4, 8 ); 
+
+const dragMesh1=new THREE.Mesh(dragGeometry1,dragMaterial)
+dragMesh1.position.z=1
+const dragMesh2=new THREE.Mesh(dragGeometry2,dragMaterial)
+dragMesh2.position.z=-1
+
+scene.add(dragMesh1,dragMesh2)
 
 
 
@@ -80,10 +97,12 @@ floor.position.y-=debug.floorSize/2
  */
 
 const camera= new THREE.PerspectiveCamera(75,sizes.width/sizes.height, 0.1 , 100)
-camera.position.x = 2
+camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 0
+camera.lookAt(new THREE.Vector3(0,0,0))
 scene.add(camera)
+
 
 /**
  * BOIDS
@@ -92,7 +111,7 @@ scene.add(camera)
 // const boidController= new BoidController(300,sizes,scene,debug,gui,camera, matCapTexture)
 const geometry = new THREE.ConeGeometry( 0.027, 0.132,3 ); 
 geometry.rotateX(-Math.PI * 0.5);
-const material = new THREE.MeshToonMaterial();
+const material = new THREE.MeshBasicMaterial({wireframe:true});
 const boidMesh= new THREE.Mesh(geometry,material)
 scene.add(boidMesh)
 /**
@@ -102,9 +121,11 @@ debug.rayPoints= 500
 debug.rayCutoff=0.5
 
 let raySpherePointPositions=RAYS.fibonacci_sphere(debug.rayPoints)
-let raySphereColors=RAYS.fibonacci_colours(debug.rayPoints, debug.rayCutoff)
-console.log(raySpherePointPositions)
 
+let raySphereColors=RAYS.fibonacci_colours(debug.rayPoints, debug.rayCutoff)
+// console.log(raySpherePointPositions)
+
+//set up geometry
 const pointsGeometry= new THREE.BufferGeometry()
 pointsGeometry.setAttribute('position',new THREE.BufferAttribute(raySpherePointPositions,3))
 pointsGeometry.setAttribute('color',new THREE.BufferAttribute(raySphereColors,3))
@@ -117,12 +138,69 @@ const pointsMaterial= new THREE.PointsMaterial({
 
 })
 
-const particleMesh= new THREE.Points(
-    pointsGeometry,
-    pointsMaterial
-)
-
+const particleMesh= new THREE.Points(pointsGeometry,pointsMaterial)
 scene.add(particleMesh)
+
+
+//create lines
+//create material
+
+
+
+
+
+let raysVec3Array=RAYS.fibonacci_sphere_vec3(debug.rayPoints,debug.rayCutoff)
+
+function addDebugLines(targetArr)
+{
+    const lineMaterial= new THREE.LineBasicMaterial({
+        color: 0xff00f0,
+    });
+    const baseTarget= new THREE.Vector3(0,0,0)
+    const lineArr=[]
+
+    targetArr.forEach((target)=>
+    {
+        let lineGeometry = new THREE.BufferGeometry().setFromPoints( [baseTarget,target] );
+    
+    
+        let line = new THREE.Line(lineGeometry, lineMaterial);
+        scene.add(line);
+        lineArr.push(line)
+        
+    })
+    return lineArr
+
+}
+// debug.lineArr = addDebugLines(raysVec3Array)
+
+function updateDebugLines(cutoff)
+{
+    const arr=debug.lineArr
+
+    for(const lineMesh of arr){
+        scene.remove(lineMesh)  
+        lineMesh.material.dispose()
+        lineMesh.geometry.dispose()
+        // console.log(line)
+    }
+                
+
+                
+            
+        
+
+    let temp=RAYS.fibonacci_sphere_vec3(debug.rayPoints,cutoff)
+
+    // console.log(temp.length)
+    
+    debug.lineArr=addDebugLines(temp)
+    // console.log(debug.lineArr)
+    
+
+}
+
+
 
 gui.add(debug,'rayPoints').min(0).max(4000).step(10).onChange((num)=>
     {
@@ -134,12 +212,17 @@ gui.add(debug,'rayPoints').min(0).max(4000).step(10).onChange((num)=>
 
         // particleBufferAttribute.needsUpdate = true;
     })
-gui.add(debug,'rayCutoff').min(-1).max(1).step(0.001).onChange((num)=>
+gui.add(debug,'rayCutoff').min(-1).max(1).step(0.001).onChange((cutoff)=>
     {
         
-        let raySphereColors=RAYS.fibonacci_colours(debug.rayPoints,num)
+        let raySphereColors=RAYS.fibonacci_colours(debug.rayPoints,cutoff)
         
         pointsGeometry.setAttribute('color',new THREE.BufferAttribute(raySphereColors,3))
+        
+        // console.log(debug.lineArr)
+        // updateDebugLines(cutoff)
+
+
 
         // particleBufferAttribute.needsUpdate = true;
     })
@@ -148,37 +231,13 @@ gui.add(debug,'rayCutoff').min(-1).max(1).step(0.001).onChange((num)=>
 
 
 
-/**
- * lights
- */
-debug.lightColor= "#ffffff"
-
-
-const pointLight = new THREE.PointLight( debug.lightColor, 5, 100 );
-pointLight.position.set( 0, 0, 0 );
-scene.add( pointLight );
-gui.addColor(debug,'lightColor').onChange((color)=>
-{
-    pointLight.color= new THREE.Color(color)
-})
-
-// const sphereSize = 0.3;
-// const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-// scene.add( pointLightHelper );
-debug.ambientLightColor= "#ffffff"
-
-const ambientLight = new THREE.AmbientLight( debug.ambientLightColor,0.005, 100 );
-// scene.add( ambientLight );
-gui.addColor(debug,'ambientLightColor').onChange((color)=>
-{
-    pointLight.color= new THREE.Color(color)
-})
 
 /**
  * add controls
  */
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping=true
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping=true
+
 
 /**
  * add renderer
@@ -189,15 +248,32 @@ const renderer= new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(2,window.devicePixelRatio))
 
+
+const dragControls = new DragControls( [dragMesh1,dragMesh2], camera, renderer.domElement );
+
 /**
  * annimaiton loop
  */
 
 const clock= new THREE.Clock()
+let past=0
+
 const tick =()=>
     {
 
         let elapsedTime= clock.getElapsedTime()
+
+        // let current= ((Math.round(Math.sin(elapsedTime/100)*100)))/10
+        let current= Math.round(Math.sin(elapsedTime)*100)/100
+        if(current!=past){
+            // console.log(current)
+            // updateDebugLines(current)
+
+
+        }
+        past=current
+
+
         // console.log(elapsedTime)
         // boidController.update()
 
