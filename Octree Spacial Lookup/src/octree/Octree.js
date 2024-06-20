d1082ac26a0f5c6c0733fc1cb6d8416a0c2fd30a
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-20 19:51:41 +0200

    Working lookUp functions

--- Octree Spacial Lookup/src/octree/Octree.js
+++ Octree Spacial Lookup/src/octree/Octree.js
@@ -56,45 +56,230 @@ export default class Octree
         });
     }
 
-    findObj(mesh,scene)
+    /**
+     * Recursively checks children, returns a list of THREE.JS Meshes that intersect with the provided object
+     * 
+     * @param {*} mesh 
+     * @param {*} scene 
+     * @returns 
+     */
+    getObjects(mesh,scene)
     {
+        const arr=[]
         const box= new THREE.Box3().setFromObject(mesh)
-        return this.intersectsObject(this.rootNode,box,scene)
+        // const testObj=this.intersectsObject(this.rootNode,box,scene,arr)
+        // const testLayers=this.intersectsLayers(this.rootNode,box,scene,arr)
+        const testBool=this.isIntersecting(this.rootNode,box,scene,arr)
+        // console.log(test)
+        // const unique = [...new Map(test.map(item => [item.uuid, item])).values()]
+
+        // console.log(testObj)
+        
+
+        console.log(testBool)
+
+        return ''
 
         
     }
 
+    /**
+     * Recursively checks children, returns the layers of the thee.js meshes
+     * @param {*} mesh 
+     * @param {*} scene 
+     * @returns 
+     */
+    getLayers(mesh,scene)
+    {
+        const arr=[]
+        const box= new THREE.Box3().setFromObject(mesh)
+        const testLayers=this.intersectsLayers(this.rootNode,box,scene,arr)
+        const unique = [...new Map(testLayers.map(item => [item.mask, item])).values()]
+
+
+        return [...unique]
+
+        
+    }
+
+    /**
+     * 
+     * Recursively checks children, returns true if the object is intersecting with any object
+     * @param {*} mesh 
+     * @param {*} scene 
+     * @returns 
+     */
+    intersects(mesh,scene)
+    {
+        const arr=[]
+        const box= new THREE.Box3().setFromObject(mesh)
+
+        return this.isIntersecting(this.rootNode,box,scene,arr)
+
+        
+    }
+
+    /**
+     * Recursively checks children, returns a list of THREE.JS Meshes that intersect with the provided box3
+     * @param {OctreeNode} node 
+     * @param {Box3} box 
+     * @param {Object3d} scene 
+     * @returns Three.Mesh[]
+     */
     intersectsObject(node,box,scene)
     {
         // console.log(node)
+        const array= []
 
         // if(node.isBox3)
         //check if bounding box intersects
-        if(node.nodeBounds.intersectsBox(box))
+        if(node.nodeBounds.intersectsBox(box)&&node.containsObject)
             {
+                //if node is leaf, return with world objects(if there are any)
                 if(node.children==null)
                     {
-                        console.log(box)
-                        console.log(node)
-                        this.debugDraw(node.nodeBounds,"green",scene)
-                        this.debugDraw(box,"green",scene)
-                        return true
+                        if(scene){
+                            this.debugDraw(node.nodeBounds,"green",scene)
+                            this.debugDraw(box,"red",scene)
+                        }
+                        
+                        
+                        return node.worldObjects
                     }
-                
+
+                //loop through children
                 for(let i =0; i<8; i++)
                     {
-                        if(node.childBounds[i].intersectsBox(box))
+                        //recursivly checks children that fit criteria:
+                        // - child bounds must intersect with the box
+                        // - node must contain an object
+                        if(node.childBounds[i].intersectsBox(box)&&node.containsObject)
                             {
-                                return this.intersectsObject(node.children[i],box,scene)
-                                // break
+                                //recurisve call
+                                const value= this.intersectsObject(node.children[i],box,scene)
+                                if(value)
+                                    {
+                                        //  NOTE: not sure if this is the best option. multiple calls may outweigh the O()time of map
+                                        //  figured map is pro
+                                        const unique = [...new Map(value.map(item => [item.uuid, item])).values()]
+                                        array.push(...unique)
+                                    }
+                                
                             }
                     }
             }
-        return false
+        return array
             
     }
 
+    /**
+         * Recursively checks children, returns the layers of the thee.js meshes
+         * @param {OctreeNode} node 
+         * @param {Box3} box 
+         * @param {Object3d} scene 
+         * @returns int[]
+         */
+    intersectsLayers(node,box,scene)
+    {
+        // console.log(node)
+        const array= []
 
+        // if(node.isBox3)
+        //check if bounding box intersects
+        if(node.nodeBounds.intersectsBox(box)&&node.containsObject)
+            {
+                //if node is leaf, return with world objects(if there are any)
+                if(node.children==null)
+                    {
+                        //generates box if there is a scene variable
+                        if(scene){
+                            this.debugDraw(node.nodeBounds,"green",scene)
+                            this.debugDraw(box,"red",scene)
+                        }
+                        const temp=[]
+                        
+
+                        if(node.worldObjects.length>0)
+                            {
+                                
+                                node.worldObjects.forEach((worldObj)=>{
+                                    temp.push(worldObj.layers);
+                                    })
+
+                            }
+                        
+                        return temp
+                    }
+
+                //loop through children
+                for(let i =0; i<8; i++)
+                    {
+                        //recursivly checks children that fit criteria:
+                        // - child bounds must intersect with the box
+                        // - node must contain an object
+                        if(node.childBounds[i].intersectsBox(box)&&node.containsObject)
+                            {
+                                //recurisve call
+                                const value= this.intersectsLayers(node.children[i],box,scene)
+                                if(value)
+                                    {
+                                        //  NOTE: not sure if this is the best option. multiple calls may outweigh the O()time of map
+                                        //  figured map is pro
+                                        // const unique = [...new Map(value.map(item => [item.uuid, item])).values()]
+                                        array.push(...value)
+                                    }
+                                
+                            }
+                    }
+            }
+        return array
+            
+    }
+
+    /**
+         * Recursively checks children, returns true if the object is intersecting with any object
+         *  
+         * @param {OctreeNode} node 
+         * @param {Box3} box 
+         * @param {Object3d} scene 
+         * @returns Three.Mesh[]
+         */
+    isIntersecting(node,box,scene)
+    {
+        let value =false
+        // if(node.isBox3)
+        //check if bounding box intersects
+        if(node.nodeBounds.intersectsBox(box)&&node.containsObject)
+            {
+                //if node is leaf, return with world objects(if there are any)
+                if(node.children==null)
+                    {
+                        
+                        //generates box if there is a scene variable
+                        if(scene){
+                            this.debugDraw(node.nodeBounds,"green",scene)
+                            this.debugDraw(box,"red",scene)
+                        }
+                        return true
+                    }
+
+                //loop through children
+                for(let i =0; i<8; i++)
+                    {
+                        //recursivly checks children that fit criteria:
+                        // - child bounds must intersect with the box
+                        // - node must contain an object
+                        if(node.childBounds[i].intersectsBox(box)&&node.containsObject)
+                            {
+                                //recurisve call
+                               const holder = this.isIntersecting(node.children[i],box,scene)
+                               if(holder){value=true}
+                            }
+                    }
+            }
+        return value
+            
+    }
 
 
 
