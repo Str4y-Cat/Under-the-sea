f855101e334340ce1a4df7ebc9b5683e7e661d56
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-06-30 17:53:32 +0200

    fish reorientates itself with the local z plane when there is no key input

--- Under The Sea/src/Experiance/Player Controller/Flight Controller.js
+++ Under The Sea/src/Experiance/Player Controller/Flight Controller.js
@@ -30,6 +30,8 @@ export default class FirstPersonControls {
         this.Attributes.InputPrevious = {...this.Attributes.InputCurrent};
         // console.log(this.Attributes)
 
+        this.normalizeZ=false
+        // this.inverse=new THREE.Quaternion()
 
 
 
@@ -141,6 +143,7 @@ export default class FirstPersonControls {
 
 
         this.updateQ = function (timeInSeconds){
+          this.normalizeZ=false
             // if (this.dead_) {
             //     return;
             //   }
@@ -197,11 +200,19 @@ export default class FirstPersonControls {
                 _Q.setFromAxisAngle(_A, -Math.PI * timeInSeconds * acc.y * input.axis1Side);
                 _R.multiply(_Q);
               }
-              if (input.axis2Side) {
-                _A.set(0, 0, -1);
-                _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * acc.y * input.axis2Side);
-                _R.multiply(_Q);
-              }
+              let inverse
+              if(!input.axis1Forward&&!input.axis1Side)
+                {
+                  // console.log('true')
+                  // console.log(object.quaternion.w)
+                  // inverse= new THREE.Quaternion(0,0,object.quaternion.z,object.quaternion.w).invert()
+                  this.normalizeZ=true
+                }
+              // if (input.axis2Side) {
+              //   _A.set(0, 0, -1);
+              //   _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * acc.y * input.axis2Side);
+              //   _R.multiply(_Q);
+              // }
           
               const forward = new THREE.Vector3(0, 0, 1);
               forward.applyQuaternion(_PARENT_Q);
@@ -223,6 +234,9 @@ export default class FirstPersonControls {
               sideways.multiplyScalar(velocity.x * timeInSeconds);
               updown.multiplyScalar(velocity.y * timeInSeconds);
               forward.multiplyScalar(velocity.z * timeInSeconds);
+
+
+
             //   console.log(sideways)
             //   console.log(updown)
             //   console.log(forward)
@@ -247,132 +261,150 @@ export default class FirstPersonControls {
             //   console.log(pos)
               
               this.object.position.copy(pos);
-              const euler=new THREE.Euler().setFromQuaternion(_R)
-            //   this.object.rotation.set(euler.x,euler.y,0,"XYZ")
+              // const euler=new THREE.Euler().setFromQuaternion(_R)
+              // const test= new THREE.Quaternion()
+              // _R.z=0
+              // this.object.quaternion.copy(finalQ);
+              if(this.normalizeZ&&this.object.quaternion.z!=0)
+                {
+                  
+                  const newQ=new THREE.Quaternion(this.object.quaternion.x,this.object.quaternion.y,0,this.object.quaternion.w)
+                  _R.rotateTowards ( newQ, 0.01 )
+                }
               this.object.quaternion.copy(_R);
+              // const test= new THREE.Vector3().copy(this.object.position)
+              // test.y+=2
+              // console.log(_R)
+              // console.log(test)
+              // this.object.lookAt(test)
             //   this.object.rotation.z=0
               this.updateAttributes()
               
             //   console.log(this.object.position)
-            //   console.log(this.object.quaternion)
+              // console.log(this.object.quaternion)
               
         };
 
-        this.updateE = function (timeInSeconds){
-            // if (this.dead_) {
-            //     return;
-            //   }
-                // console.log(this)
+        // this.updateE = function (timeInSeconds){
+        //     // if (this.dead_) {
+        //     //     return;
+        //     //   }
+        //         // console.log(this)
         
-              const input = this.Attributes.InputCurrent;
-            //   console.log(this.Attributes.InputCurrent)
+        //       const input = this.Attributes.InputCurrent;
+        //     //   console.log(this.Attributes.InputCurrent)
               
 
-              if (!input) {
-                return;
-              }
+        //       if (!input) {
+        //         return;
+        //       }
         
-              const velocity = this.velocity_;
-              const frameDecceleration = new THREE.Vector3(
-                  velocity.x * this.decceleration_.x,
-                  velocity.y * this.decceleration_.y,
-                  velocity.z * this.decceleration_.z
-              );
+        //       const velocity = this.velocity_;
+        //       const frameDecceleration = new THREE.Vector3(
+        //           velocity.x * this.decceleration_.x,
+        //           velocity.y * this.decceleration_.y,
+        //           velocity.z * this.decceleration_.z
+        //       );
               
-              frameDecceleration.multiplyScalar(timeInSeconds);
+        //       frameDecceleration.multiplyScalar(timeInSeconds);
         
-              velocity.add(frameDecceleration);
-            //   velocity.z = -math.clamp(Math.abs(velocity.z), 2, 2);
-            if(input.space){
-              velocity.z = -math.clamp(Math.abs(velocity.z), 2, 2);
+        //       velocity.add(frameDecceleration);
+        //     //   velocity.z = -math.clamp(Math.abs(velocity.z), 2, 2);
+        //     if(input.space){
+        //       velocity.z = -math.clamp(Math.abs(velocity.z), 2, 2);
 
-            }
-            else{
-                velocity.z = -math.clamp(0,0,0);     
-            }
+        //     }
+        //     else{
+        //         velocity.z = -math.clamp(0,0,0);     
+        //     }
           
-            //   console.log(object)
-              const _PARENT_Q = this.object.rotation.clone();
-              const _PARENT_P = this.object.rotation.clone();
+        //     //   console.log(object)
+        //       const _PARENT_Q = this.object.rotation.clone();
+        //       const _PARENT_P = this.object.rotation.clone();
         
-              const _Q = new THREE.Euler();
-              const _A = new THREE.Vector3();
-              const _R = _PARENT_Q.clone();
+        //       const _Q = new THREE.Euler();
+        //       const _A = new THREE.Vector3();
+        //       const _R = _PARENT_Q.clone();
           
-              const acc = this.acceleration_.clone();
-              if (input.shift) {
-                acc.multiplyScalar(2.0);
-                velocity.multiplyScalar(2.0);
-              }
+        //       const acc = this.acceleration_.clone();
+        //       if (input.shift) {
+        //         acc.multiplyScalar(2.0);
+        //         velocity.multiplyScalar(2.0);
+        //       }
           
-              if (input.axis1Forward) {
+        //       if (input.axis1Forward) {
                 
-                _Q.set(timeInSeconds * acc.y * input.axis1Forward,1,1,"XYZ");
-                _R.applyEuler(_Q);
-              }
-              if (input.axis1Side) {
-                // _A.set(0, 1, 0);
-                _Q.set(1,timeInSeconds * acc.y * input.axis1Forward,1,"XYZ");
-                _R.applyEuler(_Q);
-              }
-            //   if (input.axis2Side) {
-            //     _A.set(0, 0, -1);
-            //     _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * acc.y * input.axis2Side);
-            //     _R.multiply(_Q);
-            //   }
+        //         _Q.set(timeInSeconds * acc.y * input.axis1Forward,1,1,"XYZ");
+        //         _R.applyEuler(_Q);
+        //       }
+        //       if (input.axis1Side) {
+        //         // _A.set(0, 1, 0);
+        //         _Q.set(1,timeInSeconds * acc.y * input.axis1Forward,1,"XYZ");
+        //         _R.applyEuler(_Q);
+        //       }
+        //     //   if (input.axis2Side) {
+        //     //     _A.set(0, 0, -1);
+        //     //     _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * acc.y * input.axis2Side);
+        //     //     _R.multiply(_Q);
+        //     //   }
           
-              const forward = new THREE.Vector3(0, 0, 1);
-              forward.applyEuler(_PARENT_Q);
-              forward.normalize();
-            //   console.log(forward)
+        //       const forward = new THREE.Vector3(0, 0, 1);
+        //       forward.applyEuler(_PARENT_Q);
+        //       forward.normalize();
+        //     //   console.log(forward)
           
-              const updown = new THREE.Vector3(0, 1, 0);
-              updown.applyEuler(_PARENT_Q);
-              updown.normalize();
-            //   console.log(updown)
+        //       const updown = new THREE.Vector3(0, 1, 0);
+        //       updown.applyEuler(_PARENT_Q);
+        //       updown.normalize();
+        //     //   console.log(updown)
 
         
-              const sideways = new THREE.Vector3(1, 0, 0);
-              sideways.applyEuler(_PARENT_Q);
-              sideways.normalize();
-            //   console.log(sideways)
+        //       const sideways = new THREE.Vector3(1, 0, 0);
+        //       sideways.applyEuler(_PARENT_Q);
+        //       sideways.normalize();
+        //     //   console.log(sideways)
 
           
-              sideways.multiplyScalar(velocity.x * timeInSeconds);
-              updown.multiplyScalar(velocity.y * timeInSeconds);
-              forward.multiplyScalar(velocity.z * timeInSeconds);
-            //   console.log(sideways)
-            //   console.log(updown)
-            //   console.log(forward)
+        //       sideways.multiplyScalar(velocity.x * timeInSeconds);
+        //       updown.multiplyScalar(velocity.y * timeInSeconds);
+        //       forward.multiplyScalar(velocity.z * timeInSeconds);
+        //     //   console.log(sideways)
+        //     //   console.log(updown)
+        //     //   console.log(forward)
 
           
-              const pos = _PARENT_P;
-            //   console.log(pos)
+        //       const pos = _PARENT_P;
+        //     //   console.log(pos)
 
-              pos.add(forward);
-            //   console.log(forward)
+        //       pos.add(forward);
+        //     //   console.log(forward)
 
-            //   console.log(pos)
+        //     //   console.log(pos)
 
-              pos.add(sideways);
-            //   console.log(sideways)
+        //       pos.add(sideways);
+        //     //   console.log(sideways)
 
-            //   console.log(pos)
+        //     //   console.log(pos)
 
-              pos.add(updown);
-            //   console.log(updown)
+        //       pos.add(updown);
+        //     //   console.log(updown)
 
-            //   console.log(pos)
+        //     //   console.log(pos)
               
-              this.object.position.copy(pos);
-              this.object.quaternion.copy(_R);
-            //   this.object.rotation.z=0
-              this.updateAttributes()
+        //       // this.object.position.copy(pos);
+        //       if(this.normalizeZ)
+        //         {
+                  
+        //           _R.multiply(this.inverse)
+        //         }
+        //       this.object.quaternion.copy(_R);
+        //     //   this.object.rotation.z=0
+        //       this.updateAttributes()
               
-            //   console.log(this.object.position)
-            //   console.log(this.object.quaternion)
+        //     //   console.log(this.object.position)
+        //       console.log(this.object.quaternion)
               
-        };
+        // };
 
         this.updateAttributes=function ()
         {
